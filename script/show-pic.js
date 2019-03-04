a5270b599b39392f0f201ff20570a88489d4fcdb
Rogina Wang <47642070+Roginac@users.noreply.github.com>
2019-03-03 22:10:22 -0800

    continue

---
+++ script/show-pic.js
@@ -0,0 +1,16 @@
+var num = document.images.length;
+
+document.getElementById("prev").addEventListener("click",runPic);
+document.getElementById("next").addEventListener("click",runPic);
+
+function runPic(){
+    showPic(num);
+}
+
+function showPic(num){
+    for (i=0; i<num; i++){
+
+    } else {
+        i=0;
+    }
+}
---
+++ script/toggleSlide.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("#btn-menu").click(function(){
+      $("#menu").slideDown("slow");
+    });
+  });
